7097a5b248456bc5ca36ad451dc5c87719dc6380
Vann-jok <vann-jok@rambler.ru>
2024-06-17 23:45:26 +0300

    homeWork Map2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PhoneBook phoneBook=new PhoneBook();
+        phoneBook.add("Ivanov",123);
+        phoneBook.add("Petrov",456);
+        phoneBook.add("Ivanov",345);
+        phoneBook.add("Sidorov",274);
+        phoneBook.add("Ivanov",789);
+        phoneBook.add("Petrov",567);
+        phoneBook.add("Ivanov",678);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println(phoneBook.find("Ivanov"));
+        phoneBook.print();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
     }
 }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class PhoneBook {
+import java.util.*;
+
+public class PhoneBook  {
+    private  HashMap<String, ArrayList<Integer>> phoneBook = new HashMap<>();
+    private Comparator<? super Map.Entry<String, ArrayList<Integer>>> PhoneComparator;
+
+    public void add(String name, Integer phoneNum) {  //Добавление
+
+
+        if (phoneBook.containsKey(name)){
+            phoneBook.get(name).add(phoneNum);
+        }
+        else {
+            ArrayList<Integer> phone=new ArrayList<>();
+            phone.add(phoneNum);
+            phoneBook.put(name,phone);
+        }
+    }
+
+    public ArrayList<Integer> find(String name) { //  вывод номеров по имени
+
+        return phoneBook.get(name);
+    }
+
+    public  void print() { // вывод всей мапы
+        List<Map.Entry<String,ArrayList<Integer>>> entryList=new ArrayList<>(phoneBook.entrySet());
+        Collections.sort(entryList, new Comparator<Map.Entry<String, ArrayList<Integer>>>() {
+            @Override
+            public int compare(Map.Entry<String, ArrayList<Integer>> o1, Map.Entry<String, ArrayList<Integer>> o2) {
+                return o2.getValue().size()-o1.getValue().size();
+            }
+        });
+        System.out.println(entryList);
+    }
+
 }
